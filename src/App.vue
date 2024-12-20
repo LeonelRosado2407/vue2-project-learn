7c68442f3fa3b524302ca4762428a7c39f51f17a
Noe Leonel Rosado Quintal <leonelrosado2407@gmail.com>
2024-12-20 16:57:23 -0600

    init the vue2 project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LayoutComponent>
+      
+    </LayoutComponent>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LayoutComponent from './components/layout/LayoutComponent.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    LayoutComponent
+  },
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/layout/LayoutComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gray-800 w-full h-[100vh]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'LayoutComponent',
+    }
+</script>
